640e980cd1dbcd484d6733ebfd5a04d0e41cc575
moudjiefabrice <moudjiefabrice09@gmail.com>
2024-04-20 21:17:46 -0400

    reformat code of groovy file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,17 @@
 pipeline {
- agent any
- stages {
-     stage('Build') {
-       steps {
-         sh "mvn clean package"
-       }
-     }
-     stage('Deploy') {
+    agent any
+    stages {
+        stage('Build') {
             steps {
-              deploy adapters: [tomcat9(credentialsId: '40d220fb-636d-432c-9b08-a0014e6c2b45', path: '',
-              url: 'http://ec2-52-86-9-189.compute-1.amazonaws.com:9093/')],
-              contextPath: 'javaApiApp', war: '**/*.war'
+                sh "mvn clean package"
             }
-          }
-   }
+        }
+        stage('Deploy') {
+            steps {
+                deploy adapters: [tomcat9(credentialsId: '40d220fb-636d-432c-9b08-a0014e6c2b45', path: '',
+                        url: 'http://ec2-52-86-9-189.compute-1.amazonaws.com:9093/')],
+                        contextPath: 'javaApiApp', war: '**/*.war'
+            }
+        }
+    }
 }
